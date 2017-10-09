3ba02fe419ddca9514a4b9ca346ef70b0bf23792
Vadim Serdunich <symphonyviolist51@gmail.com>
2017-10-09 19:29:16 +0300

    fix template in component

--- js/components.js
+++ js/components.js
@@ -2,9 +2,7 @@ Vue.component('inputtext', {
     props: [
         'code',
     ],
-    template: `
-        <input type="text" class="form-control" :value="code" @input="updateCode($event.target.value)">      
-     `,
+    template: '<input type="text" class="form-control" :value="code" @input="updateCode($event.target.value)">',
     methods: {
         updateCode(code) {
 
